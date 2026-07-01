b1c8fcabcbc7f256f79ba6ccb1edc1666fb7b278
elmar <elmar@opslayer.com>
2026-07-01 08:54:35 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const columns = Array.from({ length: 9 }, () => new Set<string>())
+        const squares = Array.from({ length: 9 }, () => new Set<string>())
+
+        for (let row = 0; row < 9; row++) {
+            const rowSeen = new Set<string>()
+
+            for (let col = 0; col < 9; col++) {
+                const cur = board[row][col];
+                const square = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+
+                if (cur === '.') continue;
+
+                if (rowSeen.has(cur)
+                    || columns[col].has(cur)
+                    || squares[square].has(cur)
+                ) {
+                    return false;
+                }
+
+                rowSeen.add(cur);
+                columns[col].add(cur)
+                squares[square].add(cur)
+            }
+        }
+
+        return true;
+    }
+}
