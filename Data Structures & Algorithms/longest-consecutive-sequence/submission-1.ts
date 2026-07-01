2fd69b98ec6e595da4d7bbfc71d3b310f060c329
elmar <elmar@opslayer.com>
2026-07-01 09:39:10 +0200

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const set = new Set(nums);
+        
+        let res = 0;
+
+        for (let n of nums) {
+            if (!set.has(n - 1)) {
+                let count = 1;
+                let cur = n;
+
+                while (set.has(cur + 1)) {
+                    count++;
+                    cur = cur + 1;
+                }
+
+                res = Math.max(res, count);
+            }
+        }
+
+        return res;
+    }
+}
