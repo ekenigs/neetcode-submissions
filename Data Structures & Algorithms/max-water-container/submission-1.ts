4a265a34a9d44817be83ec1647230fff6d8f4a7c
elmar <elmar@opslayer.com>
2026-07-06 09:25:58 +0200

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        const n = heights.length;
+
+        if (!n) {
+            return 0;
+        }
+
+        let res = 0;
+        let l = 0
+        let r = n - 1;
+
+        while (l < r) {
+            const left = heights[l];
+            const right = heights[r];
+            const amount = (r - l) * Math.min(left, right);
+
+            res = Math.max(res, amount);
+
+            if (left > right) {
+                r--;
+            } else if (left < right) {
+                l++;
+            } else {
+                r--;
+                l++;
+            }
+        }
+
+        return res;
+    }
+}
