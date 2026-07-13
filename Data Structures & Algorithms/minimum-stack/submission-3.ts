67f7521eefd14e31820358e4be602b4c72d20ef7
elmar <elmar@opslayer.com>
2026-07-13 09:58:59 +0200

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.ts
@@ -0,0 +1,47 @@
+class MinStack {
+    min = [];
+    stack = [];
+
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+        
+        const min = this.min.at(-1);
+
+        if (min === undefined || min >= val) {
+            this.min.push(val)
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        const val = this.stack.pop();
+        const min = this.min.at(-1);
+
+        if (val === min) {
+            this.min.pop();
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        console.log({ min: this.min, stack: this.stack })
+        return this.min.at(-1);
+    }
+}
