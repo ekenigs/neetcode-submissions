1981e94a330489c214339fac2d3073467aebd3fe
elmar <elmar@opslayer.com>
2026-07-15 11:34:28 +0200

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.ts
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target: number, position: number[], speed: number[]): number {
+        /**
+         * 10 - 1 = 9; 9 / 3 = 3
+         * 10 - 4 = 6; 6 / 2 = 3
+         */
+
+        /**
+         * 10 - 4 = 6; 6 / 2 = 3
+         * 10 - 1 = 9; 9 / 2 = 4,5
+         * 10; 10;
+         * 3; 3;
+         */
+
+        let res = 0;
+
+        const sorted = position
+            .map((p,i) => [p, (target - p) / speed[i]])
+            .sort(([a], [b]) => a - b);
+        let curTime = -Infinity;
+
+
+        
+        while (sorted.length) {
+            const [_, t] = sorted.pop();
+
+            if (t > curTime) {
+                curTime = t;
+                res++;
+            }    
+        }
+
+        return res;
+    }
+}
