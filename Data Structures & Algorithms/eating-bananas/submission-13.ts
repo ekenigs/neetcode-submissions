35874db224ec894d76bcc1c2fc65d1433f5e9b53
elmar <elmar@opslayer.com>
2026-07-29 10:33:07 +0200

    Add: eating-bananas - submission-13

---
+++ Data Structures & Algorithms/eating-bananas/submission-13.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles: number[], h: number): number {
+        const n = piles.length;
+        
+        let left = 1;
+        let right = Math.max(...piles);
+        let k = right;
+
+        while (left <= right) {
+            const mid = right - Math.floor((right - left) / 2);
+
+            let hours = 0;
+
+            for (let p of piles) {
+                hours += Math.ceil(p / mid);
+            }
+
+            if (hours <= h && mid < k) {
+                k = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return k;
+    }
+}
